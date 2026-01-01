eb3b4ffedae1885ebc7c48d0905b70f6343bc34a
magnet-cigarettes <bontanglestari5@gmail.com>
2026-01-01 16:52:01 +0700

    commit

--- source.cpp
+++ source.cpp
@@ -5,63 +5,59 @@ using namespace std;
 
 
 void create_stack(Stack &S) {
-    S.top = -1; // [cite: 115, 117]
+    S.top = -1; 
 }
 
 bool is_Empty(Stack S) {
-    return S.top == -1; // [cite: 119, 122]
+    return S.top == -1; 
 }
 
 bool is_full(Stack S) {
-    return S.top == N_STACK - 1; // [cite: 124, 126]
+    return S.top == N_STACK - 1; 
 }
 
-// a. Prosedur Push [cite: 128, 129, 130]
 void push(Stack &S, infotypeStack x) {
     if (!is_full(S)) {
         S.top++;
-        S.info[S.top] = x; // Menambahkan elemen ke posisi teratas [cite: 130]
+        S.info[S.top] = x; 
     }
 }
 
-// b. Fungsi Pop [cite: 133, 134, 136, 137]
 infotypeStack pop(Stack &S) {
    if (!is_Empty(S)) {
         infotypeStack x = S.info[S.top];
-        S.top--; // Mengurangi indeks top [cite: 136]
+        S.top--; 
         return x;
     }
-    return ""; // Mengembalikan string kosong jika stack kosong [cite: 137]
+    return ""; 
 }
 
-// c. Prosedur manageAction (Undo/Redo) [cite: 141, 142, 143, 145, 146, 147]
 void manageAction(Stack &S_Aksi, Stack &S_Redo, infotypeStack aksi) {
     if (aksi == "UNDO") {
-        infotypeStack temp = pop(S_Aksi); // Hapus dari Aksi [cite: 145]
+        infotypeStack temp = pop(S_Aksi);
         if (temp != "") {
-            push(S_Redo, temp); // Pindahkan ke Redo [cite: 145]
+            push(S_Redo, temp); 
         }
     } else if (aksi == "REDO") {
-        infotypeStack temp = pop(S_Redo); // Hapus dari Redo [cite: 146]
+        infotypeStack temp = pop(S_Redo); 
         if (temp != "") {
-            push(S_Aksi, temp); // Pindahkan kembali ke Aksi [cite: 146]
+            push(S_Aksi, temp); 
         }
     } else {
-        push(S_Aksi, aksi); // Tambahkan aksi baru [cite: 147]
-        create_stack(S_Redo); // S_Redo dikosongkan [cite: 147]
+        push(S_Aksi, aksi); 
+        create_stack(S_Redo); 
     }
 }
 
-// d. Fungsi totalDamage [cite: 154, 155, 157]
 int totalDamage(Stack &S) {
     int total = 0;
     while (!is_Empty(S)) {
-        infotypeStack aksi = pop(S); // Membaca dan mengeluarkan aksi [cite: 154]
+        infotypeStack aksi = pop(S); 
         if (aksi == "ATTACK") {
-            total += 30; // Damage ATTACK = 30 [cite: 102]
+            total += 30; 
         } else if (aksi == "DEFENSE") {
-            total += 5; // Damage DEFENSE = 5 [cite: 102]
+            total += 5; 
         }
     }
-    return total; // Mengembalikan total damage [cite: 157]
+    return total; 
 }
