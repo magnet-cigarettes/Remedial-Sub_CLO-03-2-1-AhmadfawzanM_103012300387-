0fccd00e37ea81c9c3abcd1f244e472e344ef845
magnet-cigarettes <bontanglestari5@gmail.com>
2026-01-01 16:50:12 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -10,27 +10,23 @@ int main() {
 
     cout << "--- Simulasi Game Pertarungan ---" << endl;
 
-    // Menambah aksi pemain
+    
     cout << "Pemain melakukan: ATTACK, ATTACK, DEFENSE" << endl;
     manageAction(S_Aksi, S_Redo, "ATTACK");
     manageAction(S_Aksi, S_Redo, "ATTACK");
     manageAction(S_Aksi, S_Redo, "DEFENSE");
 
-    // Simulasi UNDO
     cout << "Pemain menekan: UNDO" << endl;
-    manageAction(S_Aksi, S_Redo, "UNDO"); // DEFENSE pindah ke S_Redo
+    manageAction(S_Aksi, S_Redo, "UNDO"); 
 
-    // Simulasi REDO
     cout << "Pemain menekan: REDO" << endl;
-    manageAction(S_Aksi, S_Redo, "REDO"); // DEFENSE kembali ke S_Aksi
+    manageAction(S_Aksi, S_Redo, "REDO"); 
 
-    // Menambah aksi baru setelah Redo
     cout << "Pemain melakukan: ATTACK" << endl;
     manageAction(S_Aksi, S_Redo, "ATTACK");
 
-    // Hitung Total Damage
     cout << "\nMenghitung Total Damage..." << endl;
-    int damage = totalDamage(S_Aksi); // S_Aksi akan menjadi kosong [cite: 157]
+    int damage = totalDamage(S_Aksi); 
 
     cout << "Total Damage yang dihasilkan: " << damage << endl;
     cout << "Status Stack Aksi sekarang kosong: " << (is_Empty(S_Aksi) ? "Ya" : "Tidak") << endl;
